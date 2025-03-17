1580b396e839fde24cdebcad6a9a0c8c37b34d3b
김명준 <86913355+mjj111@users.noreply.github.com>
2025-03-17 14:26:05 +0900

    2025-03-17 코틀린 공부

--- coroutine_study/src/main/kotlin/Sixth.kt
+++ coroutine_study/src/main/kotlin/Sixth.kt
@@ -1,50 +1,60 @@
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.example.logging
-import java.util.concurrent.ConcurrentLinkedQueue
-import kotlin.concurrent.thread
+import java.util.concurrent.Executors
+import java.util.concurrent.ThreadFactory
 import kotlin.system.measureTimeMillis
 
-//Dispatcher.DEFALUT 하나로 두개의 작업 동시성 확보 Execution Time: 3982 ms(요청 1만) 24423 ms(요청 10만)
-//Dispatcher.IO로 작업에 따라 thread 할당 Execution Time: 4370 (요청 1만) 26870ms (요청 1만)
+/**
+1000번 요청에 약 3초 소요
+
+Dispatcher.DEFALUT 하나로 두개의 작업 동시성 확보 Execution Time: 3065 ms
+Dispatcher.IO로 작업에 따라 thread 할당 Execution Time: 2989 ms
+사실상 MVC를 사용하면 request에 하나의 스레드가 묶이기 때문에 delay(500)으로 계산하면 거의 딱 맞춰 떨어진다.
+주어진 스레드에서 동시성을 가지던, IO로 시키던 큰 차이는 없음을 확인했다.
+(webFlux쓰면 얼마나 빨라질까.. 궁금하네)
+ */
+
 
 fun main() {
-    logging("main에서 실행된 스레드")
-    val time = measureTimeMillis {
-        val threads = ConcurrentLinkedQueue<Thread>()
 
-        repeat(100000) {
-            val t = thread {
-                runBlocking {
-                    launch { saveToDB() }
-                    launch { notifiactionWithFCM() }
-//                    launch(Dispatchers.IO) { saveToDB() }
-//                    launch(Dispatchers.IO) { notifiactionWithFCM() }
-                }
-            }
-            threads.add(t)
+    // 톰캣 기본 스레드풀 크기 200에 맞춰, 스레드 풀을 생성
+    val TOMCAT_THREADPOOL_SIZE = 200
+    val threadFactory = ThreadFactory { r -> Thread(r, "TomcatPool-${r.hashCode()}") }
+    val executorService = Executors.newFixedThreadPool(TOMCAT_THREADPOOL_SIZE, threadFactory)
+
+    val time = measureTimeMillis {
+        val tasks = mutableListOf<java.util.concurrent.Future<*>>()
+
+        // 1,000번 작업 세팅
+        repeat(1_000) {
+            val future = executorService.submit { chatServiceDoing() }
+            tasks.add(future)
         }
 
-        threads.forEach { it.join() }
+        // 모든 작업이 종료될 때까지 대기
+        tasks.forEach { it.get() }
     }
 
     println("\nExecution Time: $time ms")
+
+    executorService.shutdown()
 }
 
-// 추가적인 IO 스레드를 생성하지 않고 현재 스레드만으로 동시성을 갖도록 하려면 지금과 같은 방법이 좋을 듯 하다.
-// SpringMVC 를 쓰고 있다면... 지금이 모양새가 좋지 않을까? (request하나에 하나의 스레드가 동작하고 있으니 어차피 해야할 거)
-// 이런식으로 외부 리소스에 접근할 경우,
-// 다른 thread(Dispatcher.IO와 같은)를 생성하거나 풀을 쓰기보다는 현재 사용하는 스레드로 동시성을 확보하는게 좋아보이는데..흠
-// DispatcherIO를 쓰는게 좋으려나.. 부하가 생기면 어떨지 더 테스트가 필요해보인다. (WebFlux 궁금해지네..)
-
-
+fun chatServiceDoing() {
+    runBlocking {
+        launch(Dispatchers.IO){ saveToDB() }
+        launch(Dispatchers.IO){ notificationWithFCM() }
+    }
+}
 suspend fun saveToDB() {
     logging("db 저장")
-    delay(1000)
+    delay(500)
 }
 
-suspend fun notifiactionWithFCM(){
+suspend fun notificationWithFCM(){
     logging("알림 전달")
-    delay(1000)
+    delay(500)
 }
